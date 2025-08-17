be298153e4588e6485c9176aa11e9b9db962d2d9
adrian5534 <81486606+adrian5534@users.noreply.github.com>
2025-08-17 19:31:44 -0400

    test update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,4 @@
-import Navbar from './components/Navbar'
-import './styles/brand.css'
-import HeroSection from './components/HeroSection'
-import Specials from './components/Specials'
-import MenuPreview from './components/MenuPreview'
-import AboutUs from './components/AboutUs'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-
 function App() {
-  return (
-    <>
-      <Navbar />
-      <main>
-        <HeroSection />
-        <Specials />
-        <MenuPreview />
-        <AboutUs />
-        <Contact />
-      </main>
-      <Footer />
-    </>
-  )
+  return <h1 style={{color: 'red'}}>Hello World</h1>
 }
-
 export default App
